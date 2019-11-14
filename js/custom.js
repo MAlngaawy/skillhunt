0fa7eea0d289f8dff13e3240f9795dbaca39af66
MAlngaawy <46047009+MAlngaawy@users.noreply.github.com>
2019-11-14 18:20:30 +0200

    finish HomePage

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,13 @@
 $(function(){
 
+  /// Scroll Top For Navbar
+
+  var navScroll =   $(".copyright").scroll()
+
+  console.log(navScroll)
+
   // Counter By Pluggin
-  $(".num").each(function(){
+  $(".counter .num").each(function(){
     $(this).numScroll({
       number: $(this).data("number"),
       time: 3000,
@@ -15,4 +21,44 @@ $(function(){
       $($(this).data("tab")).addClass("active").siblings().removeClass("active");
     })
 
+    // Start OWL Carousel
+    $(".carousel-testimonial").owlCarousel({
+      dots: true,
+      loop:true,
+      margin:10,
+      responsivClass: true,
+      responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:3,
+        },
+        1000:{
+            items:3,
+        }
+    }
+    });
+
+    $(".carousel-candidates").owlCarousel({
+      dots: true,
+      loop:true,
+      margin:20,
+      responsivClass: true,
+      responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:3,
+        },
+        1000:{
+            items:5,
+        }
+    }
+    })
+
+  
+  
+  
 })
